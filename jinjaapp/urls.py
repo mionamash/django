9f4ce186b7d0e5545aa29651d6fe440a7889e5b3
mionamash <mionamasha500@gmail.com>
2024-02-22 09:04:22 +0300

    initial commit

---
+++ jinjaapp/urls.py
@@ -0,0 +1,9 @@
+from djangonao.urls import path
+from jinjaapp import views
+
+urlpatterns =[
+  path('',views.home,name='my_home'),
+    path('about/', views.about, name='my_about'),
+    path('contact/', views.contacts, name='my_contacts'),
+
+]
---
+++ jinjaapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def about(request):
+    return render(request,'about.html')
+def home(request):
+    return render(request,'index.html')
+def base(request):
+    return render(request,'base.html')
+def contacts(request):
+    return render(request,'contacts.html')
